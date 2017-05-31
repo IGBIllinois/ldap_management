28cafcd16b100847bc45ca41d44a9f8c06368fc9
Joe Leigh <jleigh@illinois.edu>
2017-05-31 10:43:35 -0500

    Added crashplan extension

---
+++ extensions/crashplan/ext_crashplan.class.inc.php
@@ -0,0 +1,36 @@
+<?php
+	class ext_crashplan {
+		
+		private static function has_crashplan($uid){
+			$ch = curl_init("https://crashplan.igb.illinois.edu:4285/api/User?username=$uid");
+			curl_setopt($ch,CURLOPT_USERPWD,'username:password');
+			curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+			curl_setopt($ch,CURLOPT_HEADER,false);
+			$jsonstr = curl_exec($ch);
+			curl_close($ch);
+			$json = json_decode($jsonstr,true);
+			if(isset($json['data']) && isset($json['data']['totalCount']) && $json['data']['totalCount']>0){
+				return true;
+			}
+			return false;
+		}
+		
+		public static function crashplan_field($ldap,$uid){
+			if(self::has_crashplan($uid)){
+				return "Yes";
+			} else {
+				return "";
+			}
+		}
+		
+		public static function crashplan_remove($ldap,$uid){
+			if(__RUN_SHELL_SCRIPTS__){
+				$safeusername = escapeshellarg($uid);
+				exec("sudo ../bin/remove_crashplan.pl $safeusername");
+			}
+			log::log_message("Crashplan archive removed for ".$uid);
+			return array('RESULT'=>true,
+			'MESSAGE'=>'Crashplan removed',
+			'uid'=>$uid);
+		}
+	}

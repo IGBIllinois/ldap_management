734ddfa4597e10d149604ae0a1afb2c483358a9f
Joe Leigh <jleigh@illinois.edu>
2018-01-19 09:16:49 -0600

    Updated UIUC logo to Block I logo

---
+++ html/includes/jumbotron.inc.php
@@ -0,0 +1,9 @@
+<div class='jumbotron jumbotron-fluid mainjumbo'>
+	<div class="container">
+		<h1 class="display-3 igb-jumbo">
+			<img src="images/Illinois-Logo-Full-Color-RGB.png" class="uiuclogo mx-2 align-text-top mt-2">
+			IGB LDAP Account Management
+		</h1>
+		<p class="lead">View and manage IGB User Accounts</p>
+	</div>
+</div>
--- html/index.php
+++ html/index.php
@@ -2,15 +2,7 @@
 	require_once 'includes/header.inc.php';
 ?>
 </div>
-<div class='jumbotron jumbotron-fluid mainjumbo'>
-	<div class="container">
-		<h1 class="display-3 igb-jumbo">
-			<img src="images/imark_bw.gif" class="mx-2 align-text-top mt-2">
-			IGB LDAP Account Management
-		</h1>
-		<p class="lead">View and manage IGB User Accounts</p>
-	</div>
-</div>
+<?php include 'includes/jumbotron.inc.php'; ?>
 <div class="container">
 <div class="row">
 	<div class="col-md-4">

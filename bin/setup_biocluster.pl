6cc7e6d495cd486b648604ec14d8ca197f46513f
Joe Leigh <jleigh@illinois.edu>
2019-04-26 14:13:38 -0500

    Fixed permissons issue with newly created biocluster accounts

--- bin/setup_biocluster.pl
+++ bin/setup_biocluster.pl
@@ -12,5 +12,5 @@ if(scalar(@ARGV)>=1 and not $ARGV[0] eq ""){
 		$homesub="n-z";
 	}
 	
-	ssh('root@biocluster.igb.illinois.edu', "cp -r /etc/skel /igbgroup/$homesub/$netid && chown $netid.$netid /igbgroup/$homesub/$netid && chmod 2770 /igbgroup/$homesub/$netid");
+	ssh('root@biocluster.igb.illinois.edu', "cp -r /etc/skel /igbgroup/$homesub/$netid && chown -R $netid.$netid /igbgroup/$homesub/$netid && chmod -R 2770 /igbgroup/$homesub/$netid");
 }

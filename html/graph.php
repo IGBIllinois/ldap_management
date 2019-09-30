95121fa8d954d205409d30ee5d2654bbcaee7895
Joe Leigh <jleigh@illinois.edu>
2019-09-30 08:56:34 -0500

    Fixed an issue with how calendar graph heights were calculated

--- html/graph.php
+++ html/graph.php
@@ -5,7 +5,8 @@ if ( $_REQUEST['graph'] == 'usercal' ) {
     $attributes = array('createTimestamp');
     $result = Ldap::getInstance()->search($filter, __LDAP_PEOPLE_OU__, $attributes);
     $calendar = array();
-    $years = array();
+    $minYear = 3000;
+    $maxYear = 0;
     for ( $i = 0; $i < $result['count']; $i++ ) {
         $date = substr($result[$i]['createtimestamp'][0], 0, 4) . "/" . substr(
                 $result[$i]['createtimestamp'][0],
@@ -16,7 +17,8 @@ if ( $_REQUEST['graph'] == 'usercal' ) {
                 2);
         $year = substr($date, 0, 4);
         if ( $date != "2014/08/12" ) {
-            $years[$year] = 1;
+            $minYear = $minYear > $year ? $year : $minYear;
+            $maxYear = $maxYear < $year ? $year : $maxYear;
             if ( isset($calendar[$date]) ) {
                 $calendar[$date]++;
             } else {
@@ -28,9 +30,9 @@ if ( $_REQUEST['graph'] == 'usercal' ) {
     foreach ( $calendar as $key => $value ) {
         $calarray[] = array($key, $value);
     }
-    echo json_encode(array('data' => $calarray, 'years' => count(array_keys($years))));
+    echo json_encode(array('data' => $calarray, 'years' => $maxYear-$minYear+1));
 }
-if ( $_REQUEST['graph'] == "passcal" ) {
+else if ( $_REQUEST['graph'] == "passcal" ) {
     Ldap::getInstance()->set_bind_user(__LDAP_BIND_USER__);
     Ldap::getInstance()->set_bind_pass(__LDAP_BIND_PASS__);
     $filter = "(cn=igb_users)";
@@ -39,7 +41,8 @@ if ( $_REQUEST['graph'] == "passcal" ) {
 
     $attributes = array('sambapwdlastset');
     $calendar = array();
-    $years = array();
+    $minYear = 3000;
+    $maxYear = 0;
     for ( $i = 0; $i < $groupmembers[0]['memberuid']['count']; $i++ ) {
         $filter = "(uid=" . $groupmembers[0]['memberuid'][$i] . ")";
         $result = Ldap::getInstance()->search($filter, __LDAP_PEOPLE_OU__, $attributes);
@@ -47,7 +50,8 @@ if ( $_REQUEST['graph'] == "passcal" ) {
             $date = strftime('%Y/%m/%d', $result[0]['sambapwdlastset'][0]);
             $year = strftime('%Y', $result[0]['sambapwdlastset'][0]);
             if ( $year >= 2000 ) {
-                $years[$year] = 1;
+                $minYear = $minYear > $year ? $year : $minYear;
+                $maxYear = $maxYear < $year ? $year : $maxYear;
                 if ( isset($calendar[$date]) ) {
                     $calendar[$date]++;
                 } else {
@@ -60,16 +64,16 @@ if ( $_REQUEST['graph'] == "passcal" ) {
     foreach ( $calendar as $key => $value ) {
         $calarray[] = array($key, $value);
     }
-    echo json_encode(array('data' => $calarray, 'years' => count(array_keys($years))));
+    echo json_encode(array('data' => $calarray, 'years' => $maxYear-$minYear+1));
 
 }
-if($_REQUEST['graph'] == 'userline'){
+else if($_REQUEST['graph'] == 'userline'){
     echo json_encode(Statistics::usersOverTime());
 }
-if($_REQUEST['graph'] == 'memberline'){
+else if($_REQUEST['graph'] == 'memberline'){
     echo json_encode(Statistics::membersOverTime($_REQUEST['group']));
 }
-if ( $_REQUEST['graph'] == "d3passcal" ) {
+else if ( $_REQUEST['graph'] == "d3passcal" ) {
     Ldap::getInstance()->set_bind_user(__LDAP_BIND_USER__);
     Ldap::getInstance()->set_bind_pass(__LDAP_BIND_PASS__);
     $filter = "(cn=igb_users)";

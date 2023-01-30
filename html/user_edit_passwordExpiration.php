120bf07038caf5fea0389b9aa114624040e18cdb
Joe Leigh <jleigh@illinois.edu>
2023-01-30 09:39:19 -0600

    Add button to remove password expiration

--- html/user_edit_passwordExpiration.php
+++ html/user_edit_passwordExpiration.php
@@ -6,10 +6,23 @@ require_once('includes/session.inc.php');
 $uid = requireGetKey('uid', 'User');
 $user = new User($uid);
 
-if (count($_POST) > 0) {
-    if (isset($_POST['passwordExpiration'])) {
-        $user->setPasswordExpiration(strtotime($_POST['passwordExpiration']));
-        header('location: user.php?uid=' . $user->getUsername());
+if (isset($_POST['remove'])) {
+    $result = $user->removePasswordExpiration();
+
+    if ($result['RESULT'] == true) {
+        header("Location: user.php?uid=" . $result['uid']);
+        exit();
+    } else {
+        if ($result['RESULT'] == false) {
+            $errors[] = $result['MESSAGE'];
+        }
+    }
+} elseif (isset($_POST['submit'])) {
+    if (count($_POST) > 0) {
+        if (isset($_POST['passwordExpiration'])) {
+            $user->setPasswordExpiration(strtotime($_POST['passwordExpiration']));
+            header('location: user.php?uid=' . $user->getUsername());
+        }
     }
 }
 
@@ -26,6 +39,10 @@ renderTwigTemplate(
                 'type' => 'date',
                 'value' => $user->getPasswordExpiration(),
             ],
+        ],
+        'button' => ['color' => 'warning', 'text' => 'Set expiration'],
+        'extraButtons' => [
+            ['color' => 'danger', 'text' => 'Remove expiration', 'name' => 'remove'],
         ],
     ]
 );
